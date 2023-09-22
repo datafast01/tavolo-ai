3d2dc0d759e9fb24f54497e7c97ec10dfbb0c9e3
getScaled-dev <tayyab@getscaled.com>
2023-09-23 03:46:51 +0500

    change button label

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,5 +1,3 @@
-
-
 <template>
   <div>
     <!-- Title and Logo -->
@@ -11,28 +9,14 @@
       </h1>
     </div>
 
-    <VRow
-      no-gutters
-      class="auth-wrapper"
-    >
+    <VRow no-gutters class="auth-wrapper">
       <VCol
         lg="8"
         class="d-none d-lg-flex align-center justify-center position-relative"
       >
-        <VImg
-          max-width="768px"
-          :src="authThemeImg"
-          class="auth-illustration"
-        />
-        <VImg
-          :width="276"
-          :src="tree"
-          class="auth-footer-start-tree"
-        />
-        <VImg
-          class="auth-footer-mask"
-          :src="authThemeMask"
-        />
+        <VImg max-width="768px" :src="authThemeImg" class="auth-illustration" />
+        <VImg :width="276" :src="tree" class="auth-footer-start-tree" />
+        <VImg class="auth-footer-mask" :src="authThemeMask" />
       </VCol>
 
       <VCol
@@ -40,11 +24,7 @@
         lg="4"
         class="auth-card-v2 d-flex align-center justify-center"
       >
-        <VCard
-          flat
-          :max-width="500"
-          class="mt-12 mt-sm-0 pa-4"
-        >
+        <VCard flat :max-width="500" class="mt-12 mt-sm-0 pa-4">
           <VCardText>
             <h5 class="text-h5 mb-1">
               Welcome to {{ themeConfig.app.title }}! 👋🏻
@@ -67,10 +47,7 @@
             </VAlert>
           </VCardText> -->
           <VCardText>
-            <VForm
-              ref="refVForm"
-              @submit.prevent="onSubmit"
-            >
+            <VForm ref="refVForm" @submit.prevent="onSubmit">
               <VRow>
                 <!-- email -->
                 <VCol cols="12">
@@ -91,15 +68,18 @@
                     :rules="[requiredValidator]"
                     :type="isPasswordVisible ? 'text' : 'password'"
                     :error-messages="errors.password"
-                    :append-inner-icon="isPasswordVisible ? 'mdi-eye-off-outline' : 'mdi-eye-outline'"
+                    :append-inner-icon="
+                      isPasswordVisible
+                        ? 'mdi-eye-off-outline'
+                        : 'mdi-eye-outline'
+                    "
                     @click:append-inner="isPasswordVisible = !isPasswordVisible"
                   />
 
-                  <div class="d-flex align-center flex-wrap justify-space-between mt-1 mb-4">
-                    <VCheckbox
-                      v-model="rememberMe"
-                      label="Remember me"
-                    />
+                  <div
+                    class="d-flex align-center flex-wrap justify-space-between mt-1 mb-4"
+                  >
+                    <VCheckbox v-model="rememberMe" label="Remember me" />
                     <!-- <RouterLink
                       class="text-primary ms-2 mb-1"
                       :to="{ name: 'forgot-password' }"
@@ -108,20 +88,11 @@
                     </RouterLink> -->
                   </div>
 
-                  <VBtn
-                    block
-                    type="submit"
-                    :loading="loading"
-                  >
-                    Login
-                  </VBtn>
+                  <VBtn block type="submit" :loading="loading"> Login </VBtn>
                 </VCol>
 
                 <!-- create account -->
-                <VCol
-                  cols="12"
-                  class="text-center"
-                >
+                <VCol cols="12" class="text-center">
                   <span>New on our platform?</span>
                   <RouterLink
                     class="text-primary ms-2"
@@ -129,22 +100,6 @@
                   >
                     Create an account
                   </RouterLink>
-                </VCol>
-                <VCol
-                  cols="12"
-                  class="d-flex align-center"
-                >
-                  <VDivider />
-                  <span class="mx-4">or</span>
-                  <VDivider />
-                </VCol>
-
-                auth providers
-                <VCol
-                  cols="12"
-                  class="text-center"
-                >
-                  <AuthProvider />
                 </VCol>
               </VRow>
             </VForm>
@@ -156,84 +111,93 @@
 </template>
 <script setup>
 // import { useAppAbility } from '@/plugins/casl/useAppAbility'
-import axios from '@axios'
-import { useGenerateImageVariant } from '@core/composable/useGenerateImageVariant'
-import authV2LoginIllustrationBorderedDark from '@images/pages/auth-v2-login-illustration-bordered-dark.png'
-import authV2LoginIllustrationBorderedLight from '@images/pages/auth-v2-login-illustration-bordered-light.png'
-import authV2LoginIllustrationDark from '@images/pages/auth-v2-login-illustration-dark.png'
-import authV2LoginIllustrationLight from '@images/pages/auth-v2-login-illustration-light.png'
-import authV2MaskDark from '@images/pages/auth-v2-mask-dark.png'
-import authV2MaskLight from '@images/pages/auth-v2-mask-light.png'
-import tree from '@images/pages/tree.png'
-import { VNodeRenderer } from '@layouts/components/VNodeRenderer'
-import { themeConfig } from '@themeConfig'
-import {
-emailValidator,
-requiredValidator,
-} from '@validators'
-import { ref } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
-import { VForm } from 'vuetify/components/VForm'
+import axios from "@axios";
+import { useGenerateImageVariant } from "@core/composable/useGenerateImageVariant";
+import authV2LoginIllustrationBorderedDark from "@images/pages/auth-v2-login-illustration-bordered-dark.png";
+import authV2LoginIllustrationBorderedLight from "@images/pages/auth-v2-login-illustration-bordered-light.png";
+import authV2LoginIllustrationDark from "@images/pages/auth-v2-login-illustration-dark.png";
+import authV2LoginIllustrationLight from "@images/pages/auth-v2-login-illustration-light.png";
+import authV2MaskDark from "@images/pages/auth-v2-mask-dark.png";
+import authV2MaskLight from "@images/pages/auth-v2-mask-light.png";
+import tree from "@images/pages/tree.png";
+import { VNodeRenderer } from "@layouts/components/VNodeRenderer";
+import { themeConfig } from "@themeConfig";
+import { emailValidator, requiredValidator } from "@validators";
+import { ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { VForm } from "vuetify/components/VForm";
 
-const isPasswordVisible = ref(false)
-const authThemeImg = useGenerateImageVariant(authV2LoginIllustrationLight, authV2LoginIllustrationDark, authV2LoginIllustrationBorderedLight, authV2LoginIllustrationBorderedDark, true)
-const authThemeMask = useGenerateImageVariant(authV2MaskLight, authV2MaskDark)
-const route = useRoute()
-const router = useRouter()
+const isPasswordVisible = ref(false);
+const authThemeImg = useGenerateImageVariant(
+  authV2LoginIllustrationLight,
+  authV2LoginIllustrationDark,
+  authV2LoginIllustrationBorderedLight,
+  authV2LoginIllustrationBorderedDark,
+  true
+);
+const authThemeMask = useGenerateImageVariant(authV2MaskLight, authV2MaskDark);
+const route = useRoute();
+const router = useRouter();
 // const ability = useAppAbility()
 
 const errors = ref({
   email: undefined,
   password: undefined,
-})
+});
 
-const refVForm = ref()
-const email = ref('')
-const password = ref('')
-const rememberMe = ref(false)
-const loading = ref(false)
+const refVForm = ref();
+const email = ref("");
+const password = ref("");
+const rememberMe = ref(false);
+const loading = ref(false);
 
 const login = () => {
-  loading.value = true
-  axios.post('/login', {
-    email: email.value,
-    password: password.value,
-  }).then(res => {
-console.log(res.data)
+  loading.value = true;
+  axios
+    .post("/login", {
+      email: email.value,
+      password: password.value,
+    })
+    .then((res) => {
+      console.log(res.data);
 
-    const  userData = res.data
-loading.value = false
-    // localStorage.setItem('userAbilities', JSON.stringify(userAbilities))
-   
-    localStorage.setItem('userData', JSON.stringify(userData))
-    // localStorage.setItem('accessToken', JSON.stringify(accessToken))
-if(res.status == 200){
-  router.replace(route.query.to ? String(route.query.to) : '/')
-}
-    // Redirect to `to` query if exist or redirect to index route
-    // router.replace(route.query.to ? String(route.query.to) : '/')
-  }).catch(e => {
-    loading.value = false
-    console.error(e)
-  })
-}
+      const userData = res.data;
+      loading.value = false;
+      // localStorage.setItem('userAbilities', JSON.stringify(userAbilities))
+
+      localStorage.setItem("userData", JSON.stringify(userData));
+      // localStorage.setItem('accessToken', JSON.stringify(accessToken))
+      if (res.status == 200) {
+        router.replace(route.query.to ? String(route.query.to) : "/");
+      }
+      // Redirect to `to` query if exist or redirect to index route
+      // router.replace(route.query.to ? String(route.query.to) : '/')
+    })
+    .catch((e) => {
+      loading.value = false;
+      console.error(e);
+    });
+};
 
 const onSubmit = () => {
   refVForm.value?.validate().then(({ valid: isValid }) => {
-    if (isValid)
-      login()
-  })
-}
+    if (isValid) login();
+  });
+};
 </script>
 <style lang="scss" scoped>
 @use "@core/scss/template/pages/page-auth.scss";
-.v-img__img, .v-img__picture, .v-img__gradient, .v-img__placeholder, .v-img__error {
-    z-index: -1;
-    position: absolute;
-    top: 0;
-    left: 24px;
-    width: 100%;
-    height: 100%;
+.v-img__img,
+.v-img__picture,
+.v-img__gradient,
+.v-img__placeholder,
+.v-img__error {
+  z-index: -1;
+  position: absolute;
+  top: 0;
+  left: 24px;
+  width: 100%;
+  height: 100%;
 }
 </style>
 
