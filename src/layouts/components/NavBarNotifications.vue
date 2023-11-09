3975b0f37ef789cec215133306ad51f10fb6134b
getScaled-dev <tayyab@getscaled.com>
2023-11-09 07:01:39 +0500

    worked on text editor

--- src/layouts/components/NavBarNotifications.vue
+++ src/layouts/components/NavBarNotifications.vue
@@ -1,82 +1,77 @@
 <script setup>
-import avatar3 from '@images/avatars/avatar-3.png'
-import avatar4 from '@images/avatars/avatar-4.png'
-import avatar5 from '@images/avatars/avatar-5.png'
-import paypal from '@images/svg/paypal.svg'
-
+import avatar5 from "@images/avatars/avatar-5.png";
+const date = new Date();
+const month = date.getMonth();
+const day = date.getDay();
 const notifications = ref([
-  {
-    id: 1,
-    img: avatar4,
-    title: 'Congratulation Flora! 🎉',
-    subtitle: 'Won the monthly best seller badge',
-    time: 'Today',
-    isSeen: true,
-  },
-  {
-    id: 2,
-    text: 'Tom Holland',
-    title: 'New user registered.',
-    subtitle: '5 hours ago',
-    time: 'Yesterday',
-    isSeen: false,
-  },
+  // {
+  //   id: 1,
+  //   img: avatar4,
+  //   title: 'Congratulation Flora! 🎉',
+  //   subtitle: 'Won the monthly best seller badge',
+  //   time: 'Today',
+  //   isSeen: true,
+  // },
+  // {
+  //   id: 2,
+  //   text: 'Tom Holland',
+  //   title: 'New user registered.',
+  //   subtitle: '5 hours ago',
+  //   time: 'Yesterday',
+  //   isSeen: false,
+  // },
   {
     id: 3,
     img: avatar5,
-    title: 'New message received 👋🏻',
-    subtitle: 'You have 10 unread messages',
-    time: '11 Aug',
+    title: "New account registered 👋🏻",
+    subtitle: "Welcome to Tavolo.ai",
+    time: "Today",
     isSeen: true,
   },
-  {
-    id: 4,
-    img: paypal,
-    title: 'Paypal',
-    subtitle: 'Received Payment',
-    time: '25 May',
-    isSeen: false,
-    color: 'error',
-  },
-  {
-    id: 5,
-    img: avatar3,
-    title: 'Received Order 📦',
-    subtitle: 'New order received from john',
-    time: '19 Mar',
-    isSeen: true,
-  },
-])
+  // {
+  //   id: 4,
+  //   img: paypal,
+  //   title: 'Paypal',
+  //   subtitle: 'Received Payment',
+  //   time: '25 May',
+  //   isSeen: false,
+  //   color: 'error',
+  // },
+  // {
+  //   id: 5,
+  //   img: avatar3,
+  //   title: 'Received Order 📦',
+  //   subtitle: 'New order received from john',
+  //   time: '19 Mar',
+  //   isSeen: true,
+  // },
+]);
 
-const removeNotification = notificationId => {
+const removeNotification = (notificationId) => {
   notifications.value.forEach((item, index) => {
-    if (notificationId === item.id)
-      notifications.value.splice(index, 1)
-  })
-}
+    if (notificationId === item.id) notifications.value.splice(index, 1);
+  });
+};
 
-const markRead = notificationId => {
-  notifications.value.forEach(item => {
-    notificationId.forEach(id => {
-      if (id === item.id)
-        item.isSeen = true
-    })
-  })
-}
+const markRead = (notificationId) => {
+  notifications.value.forEach((item) => {
+    notificationId.forEach((id) => {
+      if (id === item.id) item.isSeen = true;
+    });
+  });
+};
 
-const markUnRead = notificationId => {
-  notifications.value.forEach(item => {
-    notificationId.forEach(id => {
-      if (id === item.id)
-        item.isSeen = false
-    })
-  })
-}
+const markUnRead = (notificationId) => {
+  notifications.value.forEach((item) => {
+    notificationId.forEach((id) => {
+      if (id === item.id) item.isSeen = false;
+    });
+  });
+};
 
-const handleNotificationClick = notification => {
-  if (!notification.isSeen)
-    markRead([notification.id])
-}
+const handleNotificationClick = (notification) => {
+  if (!notification.isSeen) markRead([notification.id]);
+};
 </script>
 
 <template>
