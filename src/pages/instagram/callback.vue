d14d37efc23ee66c3f71d70aa721e1501c9c78b0
getScaled-dev <tayyab@getscaled.com>
2023-11-15 22:40:04 +0500

    worked on text editor

--- src/pages/instagram/callback.vue
+++ src/pages/instagram/callback.vue
@@ -3,7 +3,26 @@
 </template>
 
 <script>
-export default {};
+export default {
+  mounted() {
+    if (this.$route.query.code) {
+      const codeValue = this.$route.query.code;
+
+      // Now 'codeValue' contains the value of the 'code' parameter
+      console.log("Code Value:", codeValue);
+      axios
+        .post(`instagram/token`, { code: codeValue })
+        .then((response) => {
+          if (response.status == 200) {
+            this.$router.push("/profile");
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  },
+};
 </script>
 
 <style></style>
--- src/pages/instagram/index.vue
+++ src/pages/instagram/index.vue
@@ -94,31 +94,13 @@
 </template>
 
 <script>
-import axios from "@axios";
 export default {
   data() {
     return {
       isPlaying: false,
     };
   },
-  mounted() {
-    if (this.$route.query.code) {
-      const codeValue = this.$route.query.code;
 
-      // Now 'codeValue' contains the value of the 'code' parameter
-      console.log("Code Value:", codeValue);
-      axios
-        .post(`instagram/token`, { code: codeValue })
-        .then((response) => {
-          if (response.status == 200) {
-            this.$router.push("/profile");
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-  },
   methods: {},
 };
 </script>
