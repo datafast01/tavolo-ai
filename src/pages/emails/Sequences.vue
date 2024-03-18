bacb56bdaec8c3d3934e3419205dc8309f4a3940
jdsab <bhatti43667@gmail.com>
2024-03-19 03:27:00 +0500

    upg

--- src/pages/emails/Sequences.vue
+++ src/pages/emails/Sequences.vue
@@ -13,20 +13,31 @@
               <div class="text-h6 mb-5">Live Sequences</div>
             </div>
             <VRow>
-              <VCol
-                ><v-card class="pa-6 text-center">
-                  <p class="ma-0">Every 30 Days</p>
-                </v-card>
-              </VCol>
-              <VCol>
-                <v-card class="pa-6 text-center">
-                  <p class="ma-0">Google & Yelp Reviews</p>
-                </v-card>
-              </VCol>
-              <VCol>
-                <v-card class="pa-6 text-center">
-                  <p class="ma-0">Every 60 Days</p>
-                </v-card>
+              <VCol v-for="payments in paymentMethod" :key="payments">
+                <!-- <v-badge content="✓ " color="#9155FD"> -->
+                <div
+                  :class="{
+                    'selected-card':
+                      payments.id !== currentPkg &&
+                      selectedPaymentMethods === payments.key,
+                    'pricing-card':
+                      payments.id == currentPkg &&
+                      selectedPaymentMethods == payments.key,
+                  }"
+                  class=""
+                  @click="selectedPaymentMethods = payments.key"
+                >
+                  <v-card class="pa-6 text-center">
+                    <p class="ma-0">{{ payments.icon }}</p>
+                  </v-card>
+
+                  <div
+                    class="p-2"
+                    v-if="selectedPaymentMethods == payments.key"
+                  >
+                    <!-- <span color="#9155FD" class="tab-i">✓</span> -->
+                  </div>
+                </div>
               </VCol>
             </VRow>
           </div>
@@ -35,20 +46,28 @@
               <div class="text-h6 mb-5 mt-10">Cancelled Sequences</div>
             </div>
             <VRow>
-              <VCol
-                ><v-card class="pa-6 text-center">
-                  <p class="ma-0">New Milkshake</p>
-                </v-card>
-              </VCol>
-              <VCol>
-                <v-card class="pa-6 text-center">
-                  <p class="ma-0">25% Discount Code</p>
-                </v-card>
-              </VCol>
-              <VCol>
-                <v-card class="pa-6 text-center">
-                  <p class="ma-0">Burger Relaunch</p>
-                </v-card>
+              <VCol v-for="payments in paymentMethods" :key="payments">
+                <!-- <v-badge content="✓ " color="#9155FD"> -->
+                <div
+                  :class="{
+                    'selected-card':
+                      payments.id !== currentPkg &&
+                      selectedPaymentMethod === payments.key,
+                    'pricing-card':
+                      payments.id == currentPkg &&
+                      selectedPaymentMethod == payments.key,
+                  }"
+                  class=""
+                  @click="selectedPaymentMethod = payments.key"
+                >
+                  <v-card class="pa-6 text-center">
+                    <p class="ma-0">{{ payments.icon }}</p>
+                  </v-card>
+
+                  <div class="p-2" v-if="selectedPaymentMethod == payments.key">
+                    <!-- <span color="#9155FD" class="tab-i">✓</span> -->
+                  </div>
+                </div>
               </VCol>
             </VRow>
           </div>
@@ -60,14 +79,25 @@
 </template>
 
 <script>
-import SequenceDialog from"./SequenceDialog.vue"
-
+import SequenceDialog from "./SequenceDialog.vue";
 
 export default {
   components: { SequenceDialog },
 
   data() {
     return {
+      paymentMethods: [
+        { key: "masterCard", icon: "New Milkshake" },
+        { key: "applePay", icon: "25% Discount Code" },
+        { key: "visa", icon: "Burger Relaunch" },
+      ],
+      paymentMethod: [
+        { key: "masterCard", icon: "Every 30 Days" },
+        { key: "applePay", icon: "Google & Yelp Reviews" },
+        { key: "visa", icon: "Every 60 Days" },
+      ],
+      selectedPaymentMethods: "visa",
+      selectedPaymentMethod: "visa",
       isCardEditDialogVisible: false,
     };
   },
@@ -79,4 +109,9 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.pricing-card {
+  border: 2px solid #9155fd;
+  border-radius: 10px;
+}
+</style>
