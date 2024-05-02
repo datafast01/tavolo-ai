ff6aad2e72fe03d7f510ae60108fdc286b5ef5f2
getScaled-dev <tayyab@getscaled.com>
2024-05-02 05:39:27 +0500

    fixes

--- src/pages/tiktok/index.vue
+++ src/pages/tiktok/index.vue
@@ -1,15 +1,10 @@
 <template>
-  <div>
-    <div>
+  <div class="d-flex">
+    <div class="mr-5">
       <div class="d-flex flex-column">
-        <video
-          ref="videoPlayer"
-          width="200"
-          height="300"
-          controls
-          class="video"
-        >
-          <source src="./tiktok.mp4" type="video/mp4" />
+        <video ref="videoPlayer" width="200" height="300" controls class="video">
+          <source src="https://secureupload.s3.us-west-2.amazonaws.com/socialVideos/Snaptik.app_7361159915521527082.mp4"
+            type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         <span class="mb-2">@junnorthloop</span>
@@ -23,12 +18,7 @@
 
         <div>
           <div class="d-flex align-center">
-            <v-progress-linear
-              model-value="20"
-              color="primary"
-              :height="8"
-              rounded
-            ></v-progress-linear>
+            <v-progress-linear model-value="20" color="primary" :height="8" rounded></v-progress-linear>
           </div>
         </div>
       </div>
@@ -40,12 +30,7 @@
 
         <div>
           <div class="d-flex align-center">
-            <v-progress-linear
-              model-value="50"
-              color="primary"
-              :height="8"
-              rounded
-            ></v-progress-linear>
+            <v-progress-linear model-value="50" color="primary" :height="8" rounded></v-progress-linear>
           </div>
         </div>
       </div>
@@ -57,12 +42,7 @@
 
         <div>
           <div class="d-flex align-center">
-            <v-progress-linear
-              model-value="80"
-              color="primary"
-              :height="8"
-              rounded
-            ></v-progress-linear>
+            <v-progress-linear model-value="80" color="primary" :height="8" rounded></v-progress-linear>
           </div>
         </div>
       </div>
@@ -74,12 +54,205 @@
 
         <div>
           <div class="d-flex align-center">
-            <v-progress-linear
-              model-value="10"
-              color="primary"
-              :height="8"
-              rounded
-            ></v-progress-linear>
+            <v-progress-linear model-value="10" color="primary" :height="8" rounded></v-progress-linear>
+          </div>
+        </div>
+      </div>
+
+      <!-- <div class="overlay">
+        <button @click="togglePlayPause">
+          {{ isPlaying ? "Pause" : "Play" }}
+        </button>
+      </div> -->
+    </div>
+
+    <div class="">
+      <div class="d-flex flex-column">
+        <video ref="videoPlayer" width="200" height="300" controls class="video">
+          <source src="https://secureupload.s3.us-west-2.amazonaws.com/socialVideos/Snaptik.app_7361161687594011946.mp4"
+            type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <span class="mb-2">@junnorthloop</span>
+      </div>
+
+      <div class="username mt-1">
+        <div class="d-flex justify-space-between">
+          <span class="mb-1">Reach</span>
+          <span class="d-flex justify-end">20</span>
+        </div>
+
+        <div>
+          <div class="d-flex align-center">
+            <v-progress-linear model-value="20" color="primary" :height="8" rounded></v-progress-linear>
+          </div>
+        </div>
+      </div>
+      <div class="username my-2">
+        <div class="d-flex justify-space-between">
+          <span class="mb-1">Imressions</span>
+          <span class="d-flex justify-end">50</span>
+        </div>
+
+        <div>
+          <div class="d-flex align-center">
+            <v-progress-linear model-value="50" color="primary" :height="8" rounded></v-progress-linear>
+          </div>
+        </div>
+      </div>
+      <div class="username my-2">
+        <div class="d-flex justify-space-between">
+          <span class="mb-1">Engagement</span>
+          <span class="d-flex justify-end">80</span>
+        </div>
+
+        <div>
+          <div class="d-flex align-center">
+            <v-progress-linear model-value="80" color="primary" :height="8" rounded></v-progress-linear>
+          </div>
+        </div>
+      </div>
+      <div class="username my-2">
+        <div class="d-flex justify-space-between">
+          <span class="mb-1">Shares</span>
+          <span class="d-flex justify-end">10</span>
+        </div>
+
+        <div>
+          <div class="d-flex align-center">
+            <v-progress-linear model-value="10" color="primary" :height="8" rounded></v-progress-linear>
+          </div>
+        </div>
+      </div>
+
+      <!-- <div class="overlay">
+        <button @click="togglePlayPause">
+          {{ isPlaying ? "Pause" : "Play" }}
+        </button>
+      </div> -->
+    </div>
+
+    <div class="ml-5">
+      <div class="d-flex flex-column">
+        <video ref="videoPlayer" width="200" height="300" controls class="video">
+          <source src="https://secureupload.s3.us-west-2.amazonaws.com/socialVideos/Snaptik.app_7361163568387034411.mp4"
+            type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <span class="mb-2">@junnorthloop</span>
+      </div>
+
+      <div class="username mt-1">
+        <div class="d-flex justify-space-between">
+          <span class="mb-1">Reach</span>
+          <span class="d-flex justify-end">20</span>
+        </div>
+
+        <div>
+          <div class="d-flex align-center">
+            <v-progress-linear model-value="20" color="primary" :height="8" rounded></v-progress-linear>
+          </div>
+        </div>
+      </div>
+      <div class="username my-2">
+        <div class="d-flex justify-space-between">
+          <span class="mb-1">Imressions</span>
+          <span class="d-flex justify-end">50</span>
+        </div>
+
+        <div>
+          <div class="d-flex align-center">
+            <v-progress-linear model-value="50" color="primary" :height="8" rounded></v-progress-linear>
+          </div>
+        </div>
+      </div>
+      <div class="username my-2">
+        <div class="d-flex justify-space-between">
+          <span class="mb-1">Engagement</span>
+          <span class="d-flex justify-end">80</span>
+        </div>
+
+        <div>
+          <div class="d-flex align-center">
+            <v-progress-linear model-value="80" color="primary" :height="8" rounded></v-progress-linear>
+          </div>
+        </div>
+      </div>
+      <div class="username my-2">
+        <div class="d-flex justify-space-between">
+          <span class="mb-1">Shares</span>
+          <span class="d-flex justify-end">10</span>
+        </div>
+
+        <div>
+          <div class="d-flex align-center">
+            <v-progress-linear model-value="10" color="primary" :height="8" rounded></v-progress-linear>
+          </div>
+        </div>
+      </div>
+
+      <!-- <div class="overlay">
+        <button @click="togglePlayPause">
+          {{ isPlaying ? "Pause" : "Play" }}
+        </button>
+      </div> -->
+    </div>
+
+    <div class="ml-5">
+      <div class="d-flex flex-column">
+        <video ref="videoPlayer" width="200" height="300" controls class="video">
+          <source src="https://secureupload.s3.us-west-2.amazonaws.com/socialVideos/Snaptik.app_7361546366344072491.mp4"
+            type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <span class="mb-2">@junnorthloop</span>
+      </div>
+
+      <div class="username mt-1">
+        <div class="d-flex justify-space-between">
+          <span class="mb-1">Reach</span>
+          <span class="d-flex justify-end">20</span>
+        </div>
+
+        <div>
+          <div class="d-flex align-center">
+            <v-progress-linear model-value="20" color="primary" :height="8" rounded></v-progress-linear>
+          </div>
+        </div>
+      </div>
+      <div class="username my-2">
+        <div class="d-flex justify-space-between">
+          <span class="mb-1">Imressions</span>
+          <span class="d-flex justify-end">50</span>
+        </div>
+
+        <div>
+          <div class="d-flex align-center">
+            <v-progress-linear model-value="50" color="primary" :height="8" rounded></v-progress-linear>
+          </div>
+        </div>
+      </div>
+      <div class="username my-2">
+        <div class="d-flex justify-space-between">
+          <span class="mb-1">Engagement</span>
+          <span class="d-flex justify-end">80</span>
+        </div>
+
+        <div>
+          <div class="d-flex align-center">
+            <v-progress-linear model-value="80" color="primary" :height="8" rounded></v-progress-linear>
+          </div>
+        </div>
+      </div>
+      <div class="username my-2">
+        <div class="d-flex justify-space-between">
+          <span class="mb-1">Shares</span>
+          <span class="d-flex justify-end">10</span>
+        </div>
+
+        <div>
+          <div class="d-flex align-center">
+            <v-progress-linear model-value="10" color="primary" :height="8" rounded></v-progress-linear>
           </div>
         </div>
       </div>
@@ -108,6 +281,7 @@ export default {
 .video {
   border-radius: 24px;
 }
+
 .username {
   height: 30px;
   width: 200px;
