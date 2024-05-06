b20f20a5076cc8ce4672c219057a2b95123f31f5
getScaled-dev <tayyab@getscaled.com>
2024-05-07 03:07:58 +0500

    did changes

--- src/pages/emails/CampaignPerformance.vue
+++ src/pages/emails/CampaignPerformance.vue
@@ -3,24 +3,15 @@
     <v-row>
       <v-col class="align-self-center">Total Sent Campaigns: 43</v-col>
       <v-col class="d-flex input-cutom">
-        <v-text-field
-          style="width: 200px"
-          label="Search"
-          append-inner-icon="mdi-magnify"
-          variant="solo-filled"
-        ></v-text-field>
+        <v-text-field style="width: 200px" label="Search" append-inner-icon="mdi-magnify"
+          variant="solo-filled"></v-text-field>
         <VBtn class="ml-3" size="large"> Filter </VBtn>
       </v-col>
     </v-row>
     <VCard style="border: none" class="my-4">
       <VCardTitle> Key Performance Indicators </VCardTitle>
       <VCardText class="d-flex align-center justify-space-between">
-        <v-carousel
-          hide-delimiters
-          height="400"
-          show-arrows
-          hide-delimiter-background
-        >
+        <v-carousel hide-delimiters height="400" show-arrows hide-delimiter-background>
           <template v-slot:prev="{ props }">
             <v-icon @click="props.onClick">mdi-chevron-left</v-icon>
           </template>
@@ -31,19 +22,13 @@
             <v-icon @click="props.onClick">mdi-chevron-right</v-icon>
           </template>
           <template v-for="(item, index) in slider">
-            <v-carousel-item
-              v-if="(index + 1) % columns === 1 || columns === 1"
-              :key="index"
-            >
+            <v-carousel-item v-if="(index + 1) % columns === 1 || columns === 1" :key="index">
               <v-row class="">
                 <template v-for="(n, i) in columns">
                   <template v-if="+index + i < slider.length">
                     <v-col :key="i" style="width: 50%; height: 300px">
                       <div class="mt-5">Campaign Name</div>
-                      <BarChart
-                        :chart-data="data"
-                        :chart-options="chartOptions"
-                      />
+                      <BarChart :chart-data="data" :chart-options="chartOptions" />
                     </v-col>
                   </template>
                 </template>
@@ -55,16 +40,9 @@
     </VCard>
     <!-- SECTION data table -->
 
-    <VDataTableServer
-      v-model:items-per-page="options.itemsPerPage"
-      v-model:page="options.page"
-      :items="campaigns"
-      :items-length="totalUsers"
-      :headers="headers"
-      class="rounded-0"
-      @update:options="options = $event"
-      :loading="isLoading"
-    >
+    <VDataTableServer v-model:items-per-page="options.itemsPerPage" v-model:page="options.page" :items="campaigns"
+      :items-length="totalUsers" :headers="headers" class="rounded-0" @update:options="options = $event"
+      :loading="isLoading">
       <!-- Email -->
       <!-- <template #item.email="{ item }">
           <span class="text-sm">
@@ -79,45 +57,25 @@
         <div class="d-flex justify-end gap-x-6 pa-2 flex-wrap">
           <div class="d-flex align-center gap-x-2 text-sm">
             Rows Per Page:
-            <VSelect
-              v-model="options.itemsPerPage"
-              class="per-page-select text-high-emphasis"
-              variant="plain"
-              density="compact"
-              :items="[10, 20, 25, 50, 100]"
-            />
+            <VSelect v-model="options.itemsPerPage" class="per-page-select text-high-emphasis" variant="plain"
+              density="compact" :items="[10, 20, 25, 50, 100]" />
           </div>
 
           <!-- <span class="d-flex align-center text-sm me-2 text-high-emphasis">{{ paginationMeta(options, totalUsers) }}</span> -->
 
           <div class="d-flex gap-x-2 align-center me-2">
-            <VBtn
-              icon="mdi-chevron-left"
-              class="flip-in-rtl"
-              variant="text"
-              density="comfortable"
-              color="default"
-              :disabled="options.page <= 1"
-              @click="options.page <= 1 ? (options.page = 1) : options.page"
-            />
+            <VBtn icon="mdi-chevron-left" class="flip-in-rtl" variant="text" density="comfortable" color="default"
+              :disabled="options.page <= 1" @click="options.page <= 1 ? (options.page = 1) : options.page" />
 
-            <VBtn
-              icon="mdi-chevron-right"
-              class="flip-in-rtl"
-              density="comfortable"
-              variant="text"
-              color="default"
-              :disabled="
-                options.page >= Math.ceil(totalUsers / options.itemsPerPage)
-              "
-              @click="
-                options.page >= Math.ceil(totalUsers / options.itemsPerPage)
-                  ? (options.page = Math.ceil(
+            <VBtn icon="mdi-chevron-right" class="flip-in-rtl" density="comfortable" variant="text" color="default"
+              :disabled="options.page >= Math.ceil(totalUsers / options.itemsPerPage)
+                " @click="
+                  options.page >= Math.ceil(totalUsers / options.itemsPerPage)
+                    ? (options.page = Math.ceil(
                       totalUsers / options.itemsPerPage
                     ))
-                  : options.page++
-              "
-            />
+                    : options.page++
+                  " />
           </div>
         </div>
       </template>
@@ -130,6 +88,7 @@ import BarChart from "@/@core/libs/chartjs/components/BarChart";
 import { getLatestBarChartConfig } from "@core/libs/chartjs/chartjsConfig";
 import { useTheme } from "vuetify";
 import { VDataTableServer } from "vuetify/lib/components/index.mjs";
+import axios from "@axios";
 // import { paginationMeta } from '@/@fake-db/utils'
 // import AddNewUserDrawer from "@/views/apps/user/list/AddNewUserDrawer.vue";
 
@@ -152,38 +111,7 @@ const slider = [
   "blue-grey",
 ];
 const columns = 4;
-const campaigns = [
-  {
-    campaignName: "Campaign 1",
-    date: "01/01/2024",
-    edited: false,
-    segment: "Segment 1",
-  },
-  {
-    campaignName: "Campaign 2",
-    date: "01/02/2024",
-    edited: false,
-    segment: "Segment 2",
-  },
-  {
-    campaignName: "Campaign 3",
-    date: "01/03/2024",
-    edited: true,
-    segment: "Segment 3",
-  },
-  {
-    campaignName: "Campaign 4",
-    date: "01/04/2024",
-    edited: false,
-    segment: "Segment 4",
-  },
-  {
-    campaignName: "Campaign 5",
-    date: "01/05/2024",
-    edited: true,
-    segment: "Segment 5",
-  },
-];
+const campaigns = ref([]);
 
 let show = ref(false);
 let snkMsg = ref("");
@@ -228,6 +156,22 @@ const vuetifyTheme = useTheme();
 const chartOptions = computed(() =>
   getLatestBarChartConfig(vuetifyTheme.current.value)
 );
+
+const getEamils = (item) => {
+  axios
+    .get(
+      `dashboard/list-schedule-email?pageNo=1&pageSize=10`
+    )
+    .then((response) => {
+      show.value = true;
+      campaigns.value = response.data.data
+      console.log(response)
+    })
+    .catch((err) => {
+      console.log(err.response.status);
+    });
+};
+getEamils()
 
 const data = {
   labels: ["", "", ""],
